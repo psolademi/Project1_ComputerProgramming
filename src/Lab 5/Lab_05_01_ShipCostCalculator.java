287bd80d59db5e4d0471bccd2e6cd4e82e56ed86
Owner <Owner@macbook-air-2.lan>
2023-02-12 15:13:42 -0500

    Added Comment

--- src/Lab 5/Lab_05_01_ShipCostCalculator.java
+++ src/Lab 5/Lab_05_01_ShipCostCalculator.java
@@ -1,3 +1,4 @@
+//importing scanner
 import java.util.Scanner;
 public class Lab_05_01_ShipCostCalculator {
     public static void main(String[] args)
@@ -6,12 +7,16 @@ public class Lab_05_01_ShipCostCalculator {
         double shipping=0.0;
         System.out.print("Item price: $");
         double price = sc.nextDouble();
+        // Calculating the shipping fee if the price is less than $100
         if(price < 100)
             shipping = 0.02 * price;
+        // Cost of the item + shipping
         double totalCost = shipping + price;
+        // But if the shipping is greater than 100, return, "shipping is free"
         if(shipping == 0)
             System.out.println("Shipping is free");
         else
+            // Print the total price.
             System.out.println("Shipping Cost is: $"+shipping);
         System.out.println("Total Cost is: $"+totalCost);
     }
